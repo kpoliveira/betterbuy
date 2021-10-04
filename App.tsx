bbac4dc0f44ce0344433ab585d9b0f560cf05b5a
kpoliveira <kelvin.pinheiro.oliveira@gmail.com>
2021-10-04 18:24:06 -0300

    Ajustes no consumo da API
    - Implementação de Loader enquanto consome da API
    
    - Ajuste no consumo da API

--- App.tsx
+++ App.tsx
@@ -3,36 +3,32 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getAllProducts } from './src/services/products';
 
+import Loader from './src/components/Loader/loader';
+
 export default function App() {
 
   const [state, setState] = useState({});
+  const [load, setLoad] = useState(true)
 
-  function callAPI() {
-    const teste = getAllProducts()
-      teste.then(function(resolve){
-        const result = resolve
-        // console.log('-------------------------------------')
-        // console.log(result)
-        // const result = JSON.stringify(resolve)
-        setState(result)
-        // console.log(result)
-        return result
-      })
+  async function callAPI() {
+    const teste = await getAllProducts()
+    setState(teste)
+    setLoad(false)
     }
 
     useEffect(() => {
       callAPI()
     }, []);
 
-    // console.log(`${(state as any).data[0].description}`)
-    console.log('-------------------------------------')
-    // console.log(state.data[0])
-    // console.log('oi')
-
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      {/* {state !== undefined ? <Text>{`${(state as any).data[0].description}`}</Text> : <Text>Oi</Text>} */}
+      {/* {state !== JSON.stringify(state) ? <Text>{`${(state as any)}`}</Text> : <Text>Oi</Text>} */}
+      {load == true ?
+        <Loader loading={load}/>
+        :
+        <Text>{state.data[0].description}</Text>
+      }
       {/* <Text>getAllProducts.</Text> */}
       <StatusBar style="auto" />
     </View>
